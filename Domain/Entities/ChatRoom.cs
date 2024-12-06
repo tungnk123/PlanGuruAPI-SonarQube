a5657ebfeee5451ddce40e4aa1a49acb4b161038
Nam Đàm Thành <21522354@ms.uit.edu.vn>
2024-12-06 16:12:47 +0700

    Add Models for chat

--- Domain/Entities/ChatRoom.cs
+++ Domain/Entities/ChatRoom.cs
@@ -9,11 +9,8 @@ namespace Domain.Entities
     public class ChatRoom
     {
         public Guid ChatRoomId { get; set; }
-        public Guid User1Id { get; set; }
-        public virtual User User1 { get; set; }      
-        public Guid User2Id { get; set; }
-        public virtual User User2 { get; set; }         
-        public DateTime CreatedAt { get; set; }
-        public ICollection<ChatMessage> ChatMessages { get; set; }          
+        public Guid FirstUserId { get; set; }
+        public Guid SecondUserId { get; set; }
+        public ICollection<ChatMessage> ChatMessages { get; set; }
     }
 }
