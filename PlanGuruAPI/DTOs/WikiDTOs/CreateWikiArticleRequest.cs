27499098999b37f6efdfdf8291bf91107440c7b2
Lê Khai Trí <lekhaitri.2003@gmail.com>
2024-12-10 14:24:43 +0700

    add api create wiki

---
+++ PlanGuruAPI/DTOs/WikiDTOs/CreateWikiArticleRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace PlanGuruAPI.DTOs.WikiDTOs
+{
+    public class CreateWikiArticleResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string ArticleId { get; set; }
+    }
+
+    public class CreateWikiArticleRequest
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ThumbnailImageUrl { get; set; }
+        public string AuthorId { get; set; }
+        public List<ContentSectionRequest> ContentSections { get; set; } = new();
+        public List<string> ProductIds { get; set; } = new();
+    }
+
+    public class ContentSectionRequest
+    {
+        public string SectionName { get; set; }
+        public string Content { get; set; }
+        public List<string> ImageUrls { get; set; } = new();
+    }
+}
